ee38ef0de89170bf99d361d5c4fdc053de7614aa
sajan <sajan.george@mahiti.org>
2019-05-09 15:13:45 +0530

    resources

---
+++ EMR-Middleware/src/main/java/com/emrmiddleware/resource/Resources.java
@@ -0,0 +1,20 @@
+package com.emrmiddleware.resource;
+
+
+
+public class Resources {
+    
+	public static final boolean DEV_MODE = false;//This variable is used so that certain functionalities can be enable during development alone
+    public static final String OK="OK";
+    public static final String ERROR="ERROR";
+    public static final String AUTHERROR="No Authorization";
+    public static final String DAOEXCEPTION="Exception in DAO : ";
+	public static final String CONTROLLEREXCEPTION="Exception in Controller : ";
+	public static final String UNABLETOPROCESS="unable_to_process_request";
+	public static final String SERVER_ERROR= "Unable to process request. Please try again or Contact System Administrator";
+	public static String environment="localdev";//This is if working from local dev machine
+	//public static String environment="mahitidev";//If this is being hosted at mahiti droplet instance
+	//public static String environment="intelehealthdev";//for demo.intelehealth.io
+	
+   
+}
