fc504075877d13e728641cbbe800406f386d1ff4
sajan <sajan.george@mahiti.org>
2019-06-25 12:28:04 +0530

    Ping db when connection timeout in Mysql Pool

--- EMR-Middleware/src/main/java/com/emrmiddleware/api/dto/PatientAPIDTO.java
+++ EMR-Middleware/src/main/java/com/emrmiddleware/api/dto/PatientAPIDTO.java
@@ -5,15 +5,7 @@ import java.util.ArrayList;
 public class PatientAPIDTO {
 	private String person;
 	private ArrayList<IdentifierAPIDTO> identifiers;
-	private ArrayList < AttributeAPIDTO > attributes;
 	
-	
-	public ArrayList<AttributeAPIDTO> getAttributes() {
-		return attributes;
-	}
-	public void setAttributes(ArrayList<AttributeAPIDTO> attributes) {
-		this.attributes = attributes;
-	}
 	public String getPerson() {
 		return person;
 	}
