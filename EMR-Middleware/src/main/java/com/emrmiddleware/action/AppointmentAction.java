b7aa5f446fecc34fccf4784eef62a0bbc1b29197
Satyadeep <satyadeep@intelehealth.org>
2023-03-01 18:47:36 +0530

    Updated code for EMR-Middleware-revamp-base

---
+++ EMR-Middleware/src/main/java/com/emrmiddleware/action/AppointmentAction.java
@@ -0,0 +1,68 @@
+package com.emrmiddleware.action;
+
+import com.emrmiddleware.api.APIClient;
+import com.emrmiddleware.api.RestAPI;
+import com.emrmiddleware.api.dto.AppointmentDTO;
+import com.emrmiddleware.dto.CustomAppointmentDTO;
+import com.google.gson.Gson;
+import okhttp3.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AppointmentAction {
+
+    String authString;
+
+    private final Logger logger = LoggerFactory.getLogger(EncounterAction.class);
+    APIClient apiclient;
+    RestAPI restapiintf;
+
+    //AppointmentDTO appointmentDTO;
+    public AppointmentAction(String authString) {
+        this.authString = authString;
+        apiclient = new APIClient(authString);
+        restapiintf = apiclient.getMMClient().create(RestAPI.class);
+    }
+    public boolean addAppointmentOpenMRS(ArrayList<CustomAppointmentDTO> appointmentdto) {
+        Gson gson = new Gson();
+        String val = "";
+
+
+        try {
+
+            for (CustomAppointmentDTO appointment: appointmentdto) {
+
+
+                logger.info("appointment value : " + gson.toJson(appointment));
+
+                Call<ResponseBody> addAppointment = restapiintf.addAppointment(appointment);
+                Response<ResponseBody> response = addAppointment.execute();
+                logger.info(response.message());
+                if (response.isSuccessful()) {
+                    val = response.body().string();
+                    appointment.setSyncd(true);
+                } else {
+                    val = response.errorBody().string();
+                    logger.error("REST failed : " + val);
+                    return false;
+                }
+                logger.info("Response is : " + val);
+            }
+        } catch (IOException | NullPointerException e) {
+            // TODO Auto-generated catch block
+            logger.error(e.getMessage(), e);
+            return false;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return false;
+        }
+        return true;
+
+    }
+
+}
---
+++ EMR-Middleware/src/main/java/com/emrmiddleware/dto/CustomAppointmentDTO.java
@@ -0,0 +1,221 @@
+package com.emrmiddleware.api.dto;
+
+public class CustomAppointmentDTO {
+    public String patientAge;
+    public String patientGender;
+    public String hwName;
+    public String hwAge;
+    public String hwGender;
+    int appointmentId;
+    String slotDay;
+    String slotDate;
+    int slotDuration;
+    String slotDurationUnit;
+    String slotTime;
+    String speciality;
+    String userUuid;
+    String drName;
+    String uuid;
+    String patientName;
+    String openMrsId;
+    String patientId;
+    String locationUuid;
+    String hwUUID;
+    String reason;
+    String voided;
+
+    String visitUuid;
+    private boolean syncd = true;
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getSlotDay() {
+        return slotDay;
+    }
+
+    public void setSlotDay(String slotDay) {
+        this.slotDay = slotDay;
+    }
+
+    public String getSlotDate() {
+        return slotDate;
+    }
+
+    public void setSlotDate(String slotDate) {
+        this.slotDate = slotDate;
+    }
+
+    public int getSlotDuration() {
+        return slotDuration;
+    }
+
+    public void setSlotDuration(int slotDuration) {
+        this.slotDuration = slotDuration;
+    }
+
+    public String getSlotDurationUnit() {
+        return slotDurationUnit;
+    }
+
+    public void setSlotDurationUnit(String slotDurationUnit) {
+        this.slotDurationUnit = slotDurationUnit;
+    }
+
+    public String getSlotTime() {
+        return slotTime;
+    }
+
+    public void setSlotTime(String slotTime) {
+        this.slotTime = slotTime;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public void setUserUuid(String userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public String getDrName() {
+        return drName;
+    }
+
+    public void setDrName(String drName) {
+        this.drName = drName;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getOpenMrsId() {
+        return openMrsId;
+    }
+
+    public void setOpenMrsId(String openMrsId) {
+        this.openMrsId = openMrsId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getLocationUuid() {
+        return locationUuid;
+    }
+
+    public void setLocationUuid(String locationUuid) {
+        this.locationUuid = locationUuid;
+    }
+
+    public String getHwUUID() {
+        return hwUUID;
+    }
+
+    public void setHwUUID(String hwUUID) {
+        this.hwUUID = hwUUID;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean isSyncd() {
+        return syncd;
+    }
+
+    public void setSyncd(boolean syncd) {
+        this.syncd = syncd;
+    }
+
+    public String getVoided() {
+        return voided;
+    }
+
+    public void setVoided(String voided) {
+        this.voided = voided;
+    }
+
+    public String getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(String patientAge) {
+        this.patientAge = patientAge;
+    }
+
+    public String getPatientGender() {
+        return patientGender;
+    }
+
+    public void setPatientGender(String patientGender) {
+        this.patientGender = patientGender;
+    }
+
+    public String getHwName() {
+        return hwName;
+    }
+
+    public void setHwName(String hwName) {
+        this.hwName = hwName;
+    }
+
+    public String getHwAge() {
+        return hwAge;
+    }
+
+    public void setHwAge(String hwAge) {
+        this.hwAge = hwAge;
+    }
+
+    public String getHwGender() {
+        return hwGender;
+    }
+
+    public void setHwGender(String hwGender) {
+        this.hwGender = hwGender;
+    }
+
+    public String getVisitUuid() {
+        return visitUuid;
+    }
+
+    public void setVisitUuid(String visitUuid) {
+        this.visitUuid = visitUuid;
+    }
+}
