3bd1d961457559c0f1dbd2b3e254da32856ca8f6
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2023-09-13 04:58:22 +0530

    Update WebClientFilter.java

--- EMR-Middleware/src/main/java/org/intelehealth/middleware/utils/WebClientFilter.java
+++ EMR-Middleware/src/main/java/org/intelehealth/middleware/utils/WebClientFilter.java
@@ -4,23 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-
 import reactor.core.publisher.Mono;
 
 public class WebClientFilter {
-
     private static final Logger LOG = LoggerFactory.getLogger(WebClientFilter.class);
-
-	
 	public static ExchangeFilterFunction logRequest() {
 		return ExchangeFilterFunction.ofRequestProcessor(request -> {
 			logMethodAndUrl(request);
 			logHeaders(request);
-			
 			return Mono.just(request);
 		});
 	}
-	
 	
 	private static void logHeaders(ClientRequest request) {
 		request.headers().forEach((name, values) -> {
@@ -30,18 +24,15 @@ public class WebClientFilter {
 		});
 	}
 	
-	
 	private static void logNameAndValuePair(String name, String value) {
 		LOG.debug("{}={}", name, value);
 	}
-	
 	
 	private static void logMethodAndUrl(ClientRequest request) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(request.method().name());
 		sb.append(" to ");
 		sb.append(request.url());
-		
 		LOG.debug(sb.toString());
 	}
 }
