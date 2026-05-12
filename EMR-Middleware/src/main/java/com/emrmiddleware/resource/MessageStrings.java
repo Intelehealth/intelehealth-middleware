72e0f10ad6a2f6f840a079deb73c522f90746c5d
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2026-05-12 08:07:53 +0530

    Code for TelemedKG

--- EMR-Middleware/src/main/java/com/emrmiddleware/resource/MessageStrings.java
+++ EMR-Middleware/src/main/java/com/emrmiddleware/resource/MessageStrings.java
@@ -1,10 +1,11 @@
+/*
+ * Decompiled with CFR 0.152.
+ */
 package com.emrmiddleware.resource;
 
 public class MessageStrings {
-  public static final String SERVER_ERROR =
-      "Unable to process request. Please try again or Contact System Administrator";
-
-  public static final String SUCCESS = "Success";
-
-  public static final String ERROR = "ERROR";
+    public static final String SERVER_ERROR = "Unable to process request. Please try again or Contact System Administrator";
+    public static final String SUCCESS = "Success";
+    public static final String ERROR = "ERROR";
 }
+
