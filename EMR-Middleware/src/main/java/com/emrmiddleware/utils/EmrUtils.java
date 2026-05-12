627053f13636a0629083c0fcc5e1fdf46b8e2e3f
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2026-05-12 08:09:43 +0530

    Code for TelemedKG

--- EMR-Middleware/src/main/java/com/emrmiddleware/utils/EmrUtils.java
+++ EMR-Middleware/src/main/java/com/emrmiddleware/utils/EmrUtils.java
@@ -1,3 +1,6 @@
+/*
+ * Decompiled with CFR 0.152.
+ */
 package com.emrmiddleware.utils;
 
 import java.math.BigInteger;
@@ -8,37 +11,32 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class EmrUtils {
-
-  public static Timestamp getFormatDate(String date) {
-
-    return Timestamp.valueOf(date);
-  }
-
-  public static String getCurrentTime() {
-
-    Date date = new Date();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    return dateFormat.format(date);
-  }
-
-  public static String get_SHA_512_SecurePassword(String passwordToHash, String salt) {
-    try {
-      MessageDigest md = MessageDigest.getInstance("SHA-512");
-      String input = passwordToHash + salt;
-      byte[] messageDigest = md.digest(input.getBytes());
-
-      BigInteger no = new BigInteger(1, messageDigest);
-      String hashtext = no.toString(16);
-
-      while (hashtext.length() < 32) {
-        hashtext = "0" + hashtext;
-      }
-      return hashtext;
+    public static Timestamp getFormatDate(String date) {
+        Timestamp ts = Timestamp.valueOf(date);
+        return ts;
     }
 
-    // For specifying wrong message digest algorithms
-    catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
+    public static String getCurrentTime() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(date);
     }
-  }
+
+    public static String get_SHA_512_SecurePassword(String passwordToHash, String salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            String input = passwordToHash + salt;
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
