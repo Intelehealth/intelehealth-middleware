2177acb5517790413448065ce19fdb9276de161c
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2026-06-01 23:27:26 +0530

    Refactor PushController for improved readability

--- EMR-Middleware/src/main/java/com/emrmiddleware/rest/PushController.java
+++ EMR-Middleware/src/main/java/com/emrmiddleware/rest/PushController.java
@@ -1,4 +1,3 @@
-
 package com.emrmiddleware.rest;
 
 import com.emrmiddleware.action.PushDataAction;
@@ -13,7 +12,6 @@ import com.emrmiddleware.exception.DAOException;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.treblle.spring.annotation.EnableTreblle;
 import io.swagger.annotations.Api;
 import java.io.DataOutputStream;
 import java.net.URL;
@@ -23,7 +21,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.function.Consumer;
 import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
@@ -36,22 +33,20 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Api(value="PUSH DATA")
-@Path(value="push")
-//@EnableTreblle
+@Api(value = "PUSH DATA")
+@Path(value = "push")
 public class PushController {
     private final Logger logger = LoggerFactory.getLogger(PushController.class);
     @Context
     ServletContext context;
 
-    @Path(value="pushdata")
+    @Path(value = "pushdata")
     @POST
-    @Consumes(value={"application/json"})
-    @Produces(value={"application/json"})
+    @Consumes(value = {"application/json"})
+    @Produces(value = {"application/json"})
     public Response setData(PushDataDTO pushdatadto, @Context HttpHeaders httpHeaders) {
         ResponseDTO responsedto = new ResponseDTO();
         Gson gson = new Gson();
-        String ATTRIB_VISIT_SPECIALITY = "3f296939-c6d3-4d2e-b8ca-d7f4bfd42c2d";
         String authString = null;
         try {
             AuthenticationUtil authutil = new AuthenticationUtil();
@@ -61,39 +56,33 @@ public class PushController {
             if (!isAuthenticated || authString == null) {
                 this.logger.error("No Authorization");
                 responsedto.setStatusMessage("ERROR", "No Authorization", "unable_to_process_request");
-                return Response.status((int)403).entity((Object)gson.toJson((Object)responsedto)).build();
+                return Response.status(403).entity(gson.toJson(responsedto)).build();
             }
             PushDataAction pushdataaction = new PushDataAction(authString);
             PullDataDTO pulldatadto = new PullDataDTO();
-            PushDataDTO pdd = new PushDataDTO();
-            pdd = pushdatadto;
-            ArrayList<Object> e = new ArrayList();
-            e = pdd.getEncounters();
-            if (!e.isEmpty()) {
-                ArrayList<Object> v = new ArrayList();
-                v = pdd.getVisits();
-                ArrayList eproviders = new ArrayList();
-                String providerUID = ((EncounterAPIDTO)e.get(0)).getEncounterProviders().get(0).getProvider();
+            PushDataDTO pdd = pushdatadto;
+            ArrayList<EncounterAPIDTO> e = pdd.getEncounters();
+            if (e != null && !e.isEmpty()) {
+                ArrayList<VisitAPIDTO> v = pdd.getVisits();
+                String providerUID = e.get(0).getEncounterProviders().get(0).getProvider();
                 String[][] notifiers = new String[20][3];
                 int result = 0;
                 for (int i = 0; i < v.size(); ++i) {
-                    String patientUID = ((VisitAPIDTO)v.get(i)).getPatient();
-                    String visitUID = ((VisitAPIDTO)v.get(i)).getUuid();
-                    ArrayList<Object> attribList = new ArrayList();
-                    attribList = ((VisitAPIDTO)v.get(i)).getAttributes();
+                    String patientUID = v.get(i).getPatient();
+                    String visitUID = v.get(i).getUuid();
+                    ArrayList<AttributeAPIDTO> attribList = v.get(i).getAttributes();
                     String[] speciality = new String[]{""};
-                    boolean iv = false;
-                    attribList.forEach((Consumer<Object>)((Consumer<AttributeAPIDTO>)attrib -> {
-                        if (attrib.getAttributeType().equals("3f296939-c6d3-4d2e-b8ca-d7f4bfd42c2d")) {
-                            speciality[0] = attrib.getValue();
-                        }
-                    }));
+                    if (attribList != null) {
+                        attribList.forEach(attrib -> {
+                            if (attrib.getAttributeType().equals("3f296939-c6d3-4d2e-b8ca-d7f4bfd42c2d")) {
+                                speciality[0] = attrib.getValue();
+                            }
+                        });
+                    }
                     result = this.checkVisit(visitUID, patientUID);
-                    String patientName = "";
-                    String providerString = "";
                     if (result != 0) continue;
-                    patientName = this.getPatientName(patientUID);
-                    providerString = this.findProvider(providerUID);
+                    String patientName = this.getPatientName(patientUID);
+                    String providerString = this.findProvider(providerUID);
                     notifiers[i][0] = speciality[0];
                     notifiers[i][1] = patientName;
                     notifiers[i][2] = providerString;
@@ -107,38 +96,35 @@ public class PushController {
                 responsedto.setStatus("OK");
                 responsedto.setData(pulldatadto);
             }
-        }
-        catch (DAOException e) {
-            this.logger.error("Exception in DAO : ", (Throwable)e);
+        } catch (DAOException e) {
+            this.logger.error("Exception in DAO : ", (Throwable) e);
             responsedto.setStatusMessage("ERROR", "Unable to process request. Please try again or Contact System Administrator", "unable_to_process_request");
-            return Response.status((int)500).entity((Object)gson.toJson((Object)responsedto)).build();
-        }
-        catch (Exception e) {
+            return Response.status(500).entity(gson.toJson(responsedto)).build();
+        } catch (Exception e) {
             this.logger.error("Exception in Controller : " + e.getMessage());
-            this.logger.error("AAA", (Throwable)e);
+            this.logger.error("AAA", (Throwable) e);
             responsedto.setStatusMessage("ERROR", "Unable to process request. Please try again or Contact System Administrator", "unable_to_process_request");
-            return Response.status((int)500).entity((Object)gson.toJson((Object)responsedto)).build();
+            return Response.status(500).entity(gson.toJson(responsedto)).build();
         }
-        return Response.status((int)200).entity((Object)gson.toJson((Object)responsedto)).build();
+        return Response.status(200).entity(gson.toJson(responsedto)).build();
     }
 
     private void pushNotifications(String[][] notifiers, int noOfElements) {
         String url = "https://tele.med.kg:3004/notification/push";
         try {
             URL obj = new URL(url);
-            HttpsURLConnection con = (HttpsURLConnection)obj.openConnection();
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
             con.setRequestMethod("POST");
             con.setDoOutput(true);
             con.setRequestProperty("Content-Type", "application/json");
             for (int row = 0; row < noOfElements; ++row) {
                 JsonObject mainPacket = new JsonObject();
                 JsonObject patientDetails = new JsonObject();
-                Gson gson = new Gson();
                 patientDetails.addProperty("name", notifiers[row][1]);
                 patientDetails.addProperty("provider", notifiers[row][2]);
                 mainPacket.addProperty("speciality", notifiers[row][0]);
-                mainPacket.addProperty("skipFlag", Boolean.valueOf(false));
-                mainPacket.add("patient", (JsonElement)patientDetails);
+                mainPacket.addProperty("skipFlag", false);
+                mainPacket.add("patient", (JsonElement) patientDetails);
                 System.out.println(mainPacket);
                 DataOutputStream wr = new DataOutputStream(con.getOutputStream());
                 wr.writeBytes(String.valueOf(mainPacket));
@@ -146,32 +132,27 @@ public class PushController {
                 System.out.println("Response for notification push to URL " + url + " is : " + con.getResponseCode() + "--" + con.getResponseMessage());
                 wr.close();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception caught in pushnotification ");
             e.printStackTrace();
         }
     }
 
     public String findProvider(String providerUID) {
-        String s = "select concat_ws(':',family_name, given_name)  AS provider FROM person_name where person_id = (select person_id from provider WHERE uuid=?)";
+        String s = "select concat_ws(':',family_name, given_name) AS provider FROM person_name where person_id = (select person_id from provider WHERE uuid=?)";
         String result = "";
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
-            pstmt = con.prepareStatement(s);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
+            PreparedStatement pstmt = con.prepareStatement(s);
             pstmt.setString(1, providerUID);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
             rs.next();
             result = rs.getString(1);
             rs.close();
             pstmt.close();
             con.close();
-        }
-        catch (ClassNotFoundException | SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Exception caught in findprovider ");
             e.printStackTrace();
         }
@@ -181,22 +162,18 @@ public class PushController {
     public int checkVisit(String visitUid, String patientUid) {
         String s = "SELECT COUNT(1) FROM visit WHERE uuid=? AND patient_id = (SELECT person_id from person WHERE uuid=?)";
         int result = 0;
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
-            pstmt = con.prepareStatement(s);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
+            PreparedStatement pstmt = con.prepareStatement(s);
             pstmt.setString(1, visitUid);
             pstmt.setString(2, patientUid);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
             rs.next();
             result = rs.getInt(1);
             rs.close();
             pstmt.close();
             con.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Exception caught in checkvisit ");
             e.printStackTrace();
         }
@@ -206,25 +183,20 @@ public class PushController {
     public String getPatientName(String patientUID) {
         String s = "select given_name from person_name where person_id =(select person_id from person where uuid=?)";
         String result = "";
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
-            pstmt = con.prepareStatement(s);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/openmrs", "root", "i10hi1c");
+            PreparedStatement pstmt = con.prepareStatement(s);
             pstmt.setString(1, patientUID);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
             rs.next();
             result = rs.getString(1);
             rs.close();
             pstmt.close();
             con.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Exception caught in getPatientName ");
             e.printStackTrace();
         }
         return result;
     }
 }
-
